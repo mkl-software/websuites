5b53ef756e453c94b7c05677559bb0655abfdc7e
mkl <klosiu85@gmail.com>
2014-09-04 23:33:15 +0200

    Test refacroting and test for Jenkins-Eclipse integration.

--- webdriver-suites/src/test/java/com/mkl/websuites/test/integration/LocalTestConfig.java
+++ webdriver-suites/src/test/java/com/mkl/websuites/test/integration/LocalTestConfig.java
@@ -1,7 +1,5 @@
 package com.mkl.websuites.test.integration;
 
-import com.mkl.websuites.AnnotatedSuite;
-import com.mkl.websuites.SuiteClasses;
 import com.mkl.websuites.WebSuites;
 import com.mkl.websuites.WebSuitesConfig;
 import com.mkl.websuites.WebSuitesRunner;
@@ -14,16 +12,15 @@ public class LocalTestConfig {
 			host = "http://localhost",
 			port = 90
 			
-		)
+	)
 	public static class Config {}
 
 	
 	
-	@SuiteClasses(suite = {OneBrowserTitleOnlyTest.class, NonWebSuiteTest.class})
-	public static class LocalSuite extends AnnotatedSuite {}
-	
-	
-	@WebSuitesRunner(configurationClass = Config.class, suite = {LocalSuite.class})
+	@WebSuitesRunner(
+			configurationClass = Config.class,
+			suite = {OneBrowserTitleOnlyTest.class, NonWebSuiteTest.class}
+	)
 	public static class Runner extends WebSuites {}
 	
 }
