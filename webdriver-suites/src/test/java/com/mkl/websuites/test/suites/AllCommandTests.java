c0a41aac6967efc721f01ec8bdc270b55a14c650
mkl <klosiu85@gmail.com>
2014-10-01 23:10:35 +0200

    Minor.

--- webdriver-suites/src/test/java/com/mkl/websuites/test/suites/AllCommandTests.java
+++ webdriver-suites/src/test/java/com/mkl/websuites/test/suites/AllCommandTests.java
@@ -6,13 +6,15 @@ import org.junit.runners.Suite.SuiteClasses;
 
 import com.mkl.websuites.test.integration.command.EmptyCommandIntegrationTest;
 import com.mkl.websuites.test.integration.command.GotoCommandTest;
+import com.mkl.websuites.test.integration.command.TypeCommandTest;
 
 
 
 @RunWith(Suite.class)
 @SuiteClasses({
 	EmptyCommandIntegrationTest.class,
-	GotoCommandTest.class
+	GotoCommandTest.class,
+	TypeCommandTest.class
 })
 public class AllCommandTests {
 
