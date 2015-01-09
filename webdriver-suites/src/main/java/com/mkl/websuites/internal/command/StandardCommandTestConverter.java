c469a3a4ad12e2b9430472b283959ccd193e6a62
mkl <klosiu85@gmail.com>
2015-01-09 17:22:34 +0100

    Logic and tests for checking if a scenario file contains any commands being subtest.

--- webdriver-suites/src/main/java/com/mkl/websuites/internal/command/StandardCommandTestConverter.java
+++ webdriver-suites/src/main/java/com/mkl/websuites/internal/command/StandardCommandTestConverter.java
@@ -3,6 +3,11 @@ package com.mkl.websuites.internal.command;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.assertj.core.util.VisibleForTesting;
+
+import com.mkl.websuites.internal.command.impl.flow.ControlFlowHandler;
+import com.mkl.websuites.internal.command.impl.flow.Subtestable;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import lombok.extern.slf4j.Slf4j;
@@ -13,6 +18,11 @@ import lombok.extern.slf4j.Slf4j;
 public class StandardCommandTestConverter implements CommandTestConverter {
 
 
+	
+	private List<Command> inputCommandList;
+	private String masterScenarioFileName;
+	private boolean hasSubtests;
+	
 
 	private static StandardCommandTestConverter instance = new StandardCommandTestConverter();
 
@@ -27,19 +37,76 @@ public class StandardCommandTestConverter implements CommandTestConverter {
 	public List<Test> convertCommandsToTests(final List<Command> parsedCommands,
 			final String scenarioFileName) {
 		
+		this.inputCommandList = parsedCommands;
+		this.masterScenarioFileName = scenarioFileName;
+		
+		List<Test> convertedTests;
+		
+		if (!containsSubtests()) {
+			convertedTests = convertWtihoutSubtests();
+		} else {
+			convertedTests = convertForSubtests();
+		}
+		
+		return convertedTests;
+	}
+
+
+
+	private List<Test> convertForSubtests() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+	private boolean containsSubtests() {
+		
+		hasSubtests = false;
+		
+		checkRecursivelyForSubtests(inputCommandList);
+		
+		return hasSubtests;
+	}
+
+
+
+	@VisibleForTesting
+	protected void checkRecursivelyForSubtests(List<Command> commands) {
+		
+		for (Command command : commands) {
+			
+			if (command instanceof Subtestable) {
+				if (((Subtestable) command).isSubtest()) {
+					hasSubtests = true;
+					break;
+				}
+			}
+			
+			if (command instanceof ControlFlowHandler) {
+				
+				checkRecursivelyForSubtests(((ControlFlowHandler) command).getNestedCommands());
+				
+			}
+		}
+	}
+
+
+
+	private List<Test> convertWtihoutSubtests() {
 		List<Test> tests = new ArrayList<Test>();
 		
 		Test test = new TestCase() {
 			
 			@Override
 			public String getName() {
-				return scenarioFileName;
+				return masterScenarioFileName;
 			}
 			
 			@Override
 			protected void runTest() throws Throwable {
 				
-				for (Command command : parsedCommands) {
+				for (Command command : inputCommandList) {
 				
 					log.debug("running command from " + command);
 					command.run();
--- webdriver-suites/src/test/java/com/mkl/websuites/internal/command/StandardCommandTestConverterTest.java
+++ webdriver-suites/src/test/java/com/mkl/websuites/internal/command/StandardCommandTestConverterTest.java
@@ -1,18 +1,27 @@
 package com.mkl.websuites.internal.command;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
+import static junitparams.JUnitParamsRunner.$;
+import static java.util.Arrays.asList;
 
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import mockit.Deencapsulation;
+
+import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import com.mkl.websuites.internal.command.impl.flow.RepeatControlFlowHandler;
 import com.mkl.websuites.test.unit.scenario.cmd.SampleCommand;
 
 
+@RunWith(JUnitParamsRunner.class)
 public class StandardCommandTestConverterTest {
 
 	private StandardCommandTestConverter sut = new StandardCommandTestConverter();
@@ -29,17 +38,82 @@ public class StandardCommandTestConverterTest {
 	}
 	
 	
-//	@Test
-//	public void shoulDoConversionForOneSubtestControlFlowCommand() {
-//		//given
-//		Command command = new SampleCommand("");
-//		Map<String, String> params = new HashMap<String, String>();
-//		params.put("subtest", "true");
-//		Command repeat = new RepeatControlFlowHandler(params );
-//		List<Command> commands = Arrays.asList(command, repeat, command, command);
-//		//when
-//		List<junit.framework.Test> convertedTests = sut.convertCommandsToTests(commands, "");
-//		//then
-//		assertThat(convertedTests).hasSize(3);
-//	}
+	
+	@Ignore
+	@Test
+	public void shoulDoConversionForOneSubtestControlFlowCommandWihoutNested() {
+		//given
+		Command command = new SampleCommand("");
+		Map<String, String> params = new HashMap<String, String>();
+		params.put("subtest", "true");
+		RepeatControlFlowHandler repeat = new RepeatControlFlowHandler(params);
+		List<Command> commands = Arrays.asList(command, repeat, command, command);
+		//when
+		List<junit.framework.Test> convertedTests = sut.convertCommandsToTests(commands, "");
+		//then
+		assertThat(convertedTests).hasSize(3); // before-repeat -> empty sute for repeat -> after repeat
+	}
+	
+	
+	
+	@Parameters(method = "provideCommandListsWithNoSubtests")
+	@Test
+	public void shouldNotContainSubtest(List<Command> inputList) {
+		//given inputList
+		//when
+		sut.checkRecursivelyForSubtests(inputList);
+		boolean containsSubtests = Deencapsulation.getField(sut, "hasSubtests");
+		//then
+		assertThat(containsSubtests).isFalse();
+	}
+	
+	
+	@Parameters(method = "provideCommandListsWithSubtests")
+	@Test
+	public void shouldContainSubtest(List<Command> inputList) {
+		//given inputList
+		//when
+		sut.checkRecursivelyForSubtests(inputList);
+		boolean containsSubtests = Deencapsulation.getField(sut, "hasSubtests");
+		//then
+		assertThat(containsSubtests).isTrue();
+	}
+	
+	
+	
+	@SuppressWarnings({ "unused", "serial" })
+	private Object[] provideCommandListsWithNoSubtests() {
+		return $(
+				$(asList()),
+				$(asList((Command) new SampleCommand(""), new SampleCommand(""), new SampleCommand(""))),
+				$(asList((Command) new SampleCommand(""), new SampleCommand(""), new RepeatControlFlowHandler())),
+				$(asList(new RepeatControlFlowHandler(new HashMap<String, String>() {{
+					put("subtest", "false");
+				}
+				})))
+		);
+	}
+	
+	
+	
+	@SuppressWarnings({ "unused", "serial" })
+	private Object[] provideCommandListsWithSubtests() {
+		
+		RepeatControlFlowHandler parentCommand = new RepeatControlFlowHandler(new HashMap<String, String>() {{
+			put("subtest", "false");
+		}});
+		
+		RepeatControlFlowHandler nested = new RepeatControlFlowHandler(new HashMap<String, String>() {{
+			put("subtest", "true");
+		}});
+		
+		parentCommand.setNestedCommands(asList((Command) nested));
+		
+		return $(
+				$(asList(parentCommand)),
+				$(asList(new RepeatControlFlowHandler(new HashMap<String, String>() {{
+					put("subtest", "true");
+				}})))
+				);
+	}
 }
