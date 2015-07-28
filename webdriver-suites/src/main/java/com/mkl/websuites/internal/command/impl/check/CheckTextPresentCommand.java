932ec11c286c30cc3154677c25fad7642960f432
mkl <klosiu85@gmail.com>
2015-07-28 17:59:20 +0200

    Check if element with text is present on the page.

---
+++ webdriver-suites/src/main/java/com/mkl/websuites/internal/command/impl/check/CheckTextPresentCommand.java
@@ -0,0 +1,53 @@
+package com.mkl.websuites.internal.command.impl.check;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.assertj.core.api.AbstractAssert;
+import org.assertj.core.api.ListAssert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import com.mkl.websuites.internal.command.CommandDescriptor;
+
+
+@CommandDescriptor(name = "checkText", argumentTypes = String.class)
+public class CheckTextPresentCommand extends AbstractCheck {
+
+	
+	protected String text;
+
+	public CheckTextPresentCommand(String text) {
+		this.text = text;
+	}
+
+	@Override
+	protected Object[] getAssertionsParameters() {
+		List<WebElement> list = browser.findElements(By.xpath("//body//*[contains(text(),'" + text + "')]"));
+		return new Object[] {list};
+	}
+
+	@Override
+	protected AbstractAssert<?, ?> buildAssertion(Object... args) {
+		@SuppressWarnings("unchecked")
+		List<WebElement> elementList = (List<WebElement>) args[0];
+		return assertThat(elementList);
+	}
+
+	@Override
+	protected void runAssertion(AbstractAssert<?, ?> assertion, Object... args) {
+		
+		((ListAssert<?>) assertion)
+			.overridingErrorMessage("Text '%s' is not present in any of the elements in the page" , text)
+			.isNotEmpty();
+		
+	}
+
+	
+	
+	
+	
+	
+	
+}
--- webdriver-suites/src/main/java/com/mkl/websuites/internal/command/impl/check/CheckTitleCommand.java
+++ webdriver-suites/src/main/java/com/mkl/websuites/internal/command/impl/check/CheckTitleCommand.java
@@ -32,4 +32,5 @@ public class CheckTitleCommand extends AbstractSingleStringCheck {
 			.isEqualTo(expectedTitle);
 	}
 
+
 }
