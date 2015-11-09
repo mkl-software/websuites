dc02a7ba2424734ceb3f55a4c2c2d19413a81f75
Marcin Klosinski <m.klosinski@mkl-software.com>
2015-11-09 23:53:14 +0100

    Configured Maven plugin that adds license header to every file in the project. License header applied. (WEB-37)

--- webdriver-suites/src/main/java/com/mkl/websuites/WebSuites.java
+++ webdriver-suites/src/main/java/com/mkl/websuites/WebSuites.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2015 MKL Software
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.mkl.websuites;
 
 import java.lang.annotation.Retention;
@@ -12,8 +27,12 @@ import com.mkl.websuites.config.TestClass;
 
 /**
  * Configuration for runner classes that extends {@link com.mkl.websuites.WebSuitesRunner}.
- * <p>This annotation allows to define test suites and browsers to run these tests against.</p>
- * <p>Sample configuration might look like:</p>
+ * <p>
+ * This annotation allows to define test suites and browsers to run these tests against.
+ * </p>
+ * <p>
+ * Sample configuration might look like:
+ * </p>
  * <code>
  * {@literal @}WebSuites(<br/>
  * &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;browsers = {"chrome", "ff", "ie"},<br/>
@@ -36,7 +55,7 @@ import com.mkl.websuites.config.TestClass;
  * public class MyRunner extends WebSuitesRunner {} <br/><br/>
  * </code>
  * 
-  * @author Marcin Klosinski
+ * @author Marcin Klosinski
  *
  */
 @Retention(RetentionPolicy.RUNTIME)
@@ -44,38 +63,56 @@ public @interface WebSuites {
 
     /**
      * List of browsers to run all tests suites against. By default it's an empty list.
-     * <p>The elements of this list are string identifiers of browsers, like "<b>ff</b>" or "<b>chrome</b>".</p>
-     * that come from {@link com.mkl.websuites.config.BrowserConfig{ annotation.
-     * <p>By default three identifiers are available:</p>
+     * <p>
+     * The elements of this list are string identifiers of browsers, like "<b>ff</b>" or
+     * "<b>chrome</b>".
+     * </p>
+     * that come from {@link com.mkl.websuites.config.BrowserConfig annotation.
+     * <p>
+     * By default three identifiers are available:
+     * </p>
      * <ul>
-     *      <li><b>ff</b> for default FireFox driver</li>
-     *      <li><b>html</b> for HtmlUnitDriver</li>
-     *      <li><b>none</b> for non-browser tests</li>
+     * <li><b>ff</b> for default FireFox driver</li>
+     * <li><b>html</b> for HtmlUnitDriver</li>
+     * <li><b>none</b> for non-browser tests</li>
      * </ul>
-     * <p>For custom browser definitions which use explicit web drivers please specify a corresponding element in
-     * the <code>browserConfiguration</code> element</p>
+     * <p>
+     * For custom browser definitions which use explicit web drivers please specify a corresponding
+     * element in the <code>browserConfiguration</code> element
+     * </p>
      */
     String[] browsers() default {};
 
-//    String browsersPropertyName() default ""; 
 
     /**
      * Use this attribute to define folder path from which all scenario files will be run.
-     * <p>You can specify more than one paths.</p>
-     * <p>Each folder is specified using {@link com.mkl.websuites.config.Folder} annotation.</p>
-     * <p>Example usage:</p>
+     * <p>
+     * You can specify more than one paths.
+     * </p>
+     * <p>
+     * Each folder is specified using {@link com.mkl.websuites.config.Folder} annotation.
+     * </p>
+     * <p>
+     * Example usage:
+     * </p>
      * <code>
      * folders = {@literal @}Folder(path = "/tests/allSearchTests")
      * </code>
      */
     Folder[] folders() default {};
 
-    
+
     /**
      * Use this attribute to specify explicitly which scenario files to run.
-     * <p>You can specify more than one files.</p>
-     * <p>Each folder is specified using {@link com.mkl.websuites.config.ScenarioFile} annotation.</p>
-     * <p>Example usage:</p>
+     * <p>
+     * You can specify more than one files.
+     * </p>
+     * <p>
+     * Each folder is specified using {@link com.mkl.websuites.config.ScenarioFile} annotation.
+     * </p>
+     * <p>
+     * Example usage:
+     * </p>
      * <code>
      * scenarios = {{@literal @}ScenarioFile("/tests/navigation/menu1.scn"),
      * {@literal @}ScenarioFile("/tests/navigation/menu2.scn"),
@@ -84,12 +121,18 @@ public @interface WebSuites {
      */
     ScenarioFile[] scenarios() default {};
 
-    
+
     /**
      * Use this attribute to specify explicitly which test classes to run.
-     * <p>You can specify more than one test class.</p>
-     * <p>Each test class is specified using {@link com.mkl.websuites.config.TestClass} annotation.</p>
-     * <p>Example usage:</p>
+     * <p>
+     * You can specify more than one test class.
+     * </p>
+     * <p>
+     * Each test class is specified using {@link com.mkl.websuites.config.TestClass} annotation.
+     * </p>
+     * <p>
+     * Example usage:
+     * </p>
      * <code>
      * tests = {{@literal @}TestClass(DetailedSavedSearchesTest.class),
      * {@literal @}TestClass(LoginFailureTest.class)}
@@ -99,29 +142,37 @@ public @interface WebSuites {
 
     String propertiesFileName() default "";
 
-    
+
     /**
-     * You can specify main site configuration here. In the {@link com.mkl.websuites.config.SiteConfig} annotation you
-     * can specify server host, port and base address. Then, in your tests you can access this information using
-     * properties:
+     * You can specify main site configuration here. In the
+     * {@link com.mkl.websuites.config.SiteConfig} annotation you can specify server host, port and
+     * base address. Then, in your tests you can access this information using properties:
      * <ul>
-     *      <li><code>${site}</code> - concatenated protocol, host, port nad base path</li>
-     *      <li><code>${site.protocol}</code> - protocol</li>
-     *      <li><code>${site.host}</code> - host name or IP address</li>
-     *      <li><code>${site.port}</code> - port number</li>
-     *      <li><code>${site.basePath}</code> - base context path</li>
+     * <li><code>${site}</code> - concatenated protocol, host, port nad base path</li>
+     * <li><code>${site.protocol}</code> - protocol</li>
+     * <li><code>${site.host}</code> - host name or IP address</li>
+     * <li><code>${site.port}</code> - port number</li>
+     * <li><code>${site.basePath}</code> - base context path</li>
      * </ul>
-     * <p>The most commonly used setting is <code>waitTimeout</code>, which sets the global
-     * timeout for all implicit WebDriver waits.</p>
+     * <p>
+     * The most commonly used setting is <code>waitTimeout</code>, which sets the global timeout for
+     * all implicit WebDriver waits.
+     * </p>
      */
     SiteConfig site() default @SiteConfig;
 
     /**
      * Specifies browsers that can be used in multi-browser tests.
-     * <p>Each browser is specified using {@link com.mkl.websuites.config.BrowserConfig} annotation, which
-     * determines browser local ID, dipslay name and web driver physical location.
-     * </p>By default the FireFox browser is available so if installedo on the OS it will work out-of-the-box.<p>
-     * <p>Sample configuration of custom browser:</p>
+     * <p>
+     * Each browser is specified using {@link com.mkl.websuites.config.BrowserConfig} annotation,
+     * which determines browser local ID, dipslay name and web driver physical location.
+     * </p>
+     * By default the FireFox browser is available so if installedo on the OS it will work
+     * out-of-the-box.
+     * <p>
+     * <p>
+     * Sample configuration of custom browser:
+     * </p>
      * <code>{@literal @}BrowserConifg(browserType = BrowserType.CHROME, id = "chrome", 
      * displayName = "Firefox", webDriverPath = "src/test/resources/drivers/ChromeDriver.exe"),</code>
      * 
@@ -129,24 +180,27 @@ public @interface WebSuites {
      */
     BrowserConifg[] browserConfiguration() default {};
 
-    
+
     /**
-     * If there are multiple WebSuites runner classes, you can share browser configuration between them and avoid
-     * specifying them in each runner separately. Use this class to point to a class which holds 
-     * {@link com.mkl.websuites.config.BrowserDefinition} annotation.
-     * <p>If both <code>browserConfiguration</code> and <code>browserResusableConfiguration</code> are specified,
-     * then the browser configuration is merged with a priority to <code>browserConfiguration</code> 
-     * explicit definitions.</p>
+     * If there are multiple WebSuites runner classes, you can share browser configuration between
+     * them and avoid specifying them in each runner separately. Use this class to point to a class
+     * which holds {@link com.mkl.websuites.config.BrowserDefinition} annotation.
+     * <p>
+     * If both <code>browserConfiguration</code> and <code>browserResusableConfiguration</code> are
+     * specified, then the browser configuration is merged with a priority to
+     * <code>browserConfiguration</code> explicit definitions.
+     * </p>
+     * 
      * @return
      */
     Class<?> browserResusableConfiguration() default Object.class;
 
-    
+
     /**
      * Specifies extensions and customization of the framework:
      * <ol>
-     *      <li>package paths for custom commands</li>
-     *      <li>service overrides to deeply modify framework behaviour</li>
+     * <li>package paths for custom commands</li>
+     * <li>service overrides to deeply modify framework behaviour</li>
      * </ol>
      */
     Extension extension() default @Extension;
